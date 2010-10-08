71298af63043d9ab6fc7d546081d84fdb5901ead
Blaizer <blaiseroth@gmail.com>
2010-10-08 13:58:18 +1030

    roto pooooom

--- roto.pl
+++ roto.pl
@@ -10,7 +10,19 @@ my $rad = 0;
 $app->add_move_handler(sub {
 	my $zoom = cos $rad;
 	$rad += 1 * $_[0];
-	my $s = SDL::GFX::Rotozoom::zoom_surface( $s, $zoom, 1, 0);
+	
+	#these both work for me
+	# my $s = SDL::GFX::Rotozoom::zoom_surface($s, $zoom, 1, 0);
+	my $s = SDL::GFX::Rotozoom::surface_xy($s, 0, $zoom, 1, 0);
+	
+	#these don't even have a src argument... Wut?
+	# my $s = SDL::GFX::Rotozoom::surface_size(width, height, angle, zoom);
+	# my $s = SDL::GFX::Rotozoom::surface_size_xy(width, height, angle, zoomx, zoomy);
+	# my $s = SDL::GFX::Rotozoom::zoom_surface_size(width, height, zoomx, zoomy);
+
+	
+	
+	
 	$app->draw_rect;
 	$app->blit_by($s, undef, [200 - $s->w / 2, 0]);
 	$app->flip;
